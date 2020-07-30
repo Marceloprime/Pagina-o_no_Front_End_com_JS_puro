e1b669e020ca79d26c56874b6896475400babdaa
Marcelo <marceloreis7777@gmail.com>
2020-07-30 16:22:24 -0300

    'second'

--- script.js
+++ script.js
@@ -1,13 +1,83 @@
-//Funcoes
-function populateList(){
-    //atribuicao do construct array, que inicializar o array com 100 posicoes
-    //e usa uma funcao map, que roda para cada elemento
-    const data = Array.from({length: 100}).map((_,i) => `<div class="item"> Item ${(i + 1)}</div>`)
-
-    const lista = document.querySelector('#paginate .list')
-    console.log(lista)
-    lista.innerHTML = data.join("")
-}
-
 //run
-populateList()
+function main(){
+ populateList()
+ //Funcoes
+ function populateList(){
+     //atribuicao do construct array, que inicializar o array com 100 posicoes
+     //e usa uma funcao map, que roda para cada elemento
+     const data = Array.from({length: 100}).map((_,i) => `<div class="item"> Item ${(i + 1)}</div>`)
+ 
+     const lista = document.querySelector('#paginate .list')
+     lista.innerHTML = data.join("")
+ 
+     return data
+ }
+ 
+ const data = populateList()
+ let perPage = 3
+ //Guarda o estado da pagina
+ const state = {
+     page: 1,
+     perPage,
+     totalPage: Math.ceil(data.length / perPage)
+ }
+ 
+ const html = {
+     get(element){
+         console.log('oi')
+         return document.querySelector(element)
+     }
+ }
+ 
+ //objeto de funçoes
+ const controls = {
+     next() {
+         state.page++
+         const lastPage = state.page > state.totalPage
+         if(lastPage){
+             state.page--
+         }
+     },
+     prev() {
+         state.page--
+         if(state.page < 1){
+             state.page++
+         }
+     },
+     goTo(page) {
+         if(page < 1){
+             page = 1
+         }
+         state.page = page
+ 
+         if(page > state.totalPage){
+             state.page = state.totalPage
+         }
+     },
+     createListeners(){
+        html.get('.first').addEventListener('click',()=>{
+            controls.goTo(1)
+            update()
+        })
+
+        html.get('.last').addEventListener('click',()=>{
+            controls.goTo(state.totalPage)
+            update()
+        })
+
+        html.get('.next').addEventListener('click',()=>{
+            controls.next()
+            update()
+        })
+
+        html.get('.prev').addEventListener('click',()=>{
+            controls.prev()
+            update()
+        })
+     }
+ }
+
+ function update(){
+     console.log(state.page)
+ }
+}
