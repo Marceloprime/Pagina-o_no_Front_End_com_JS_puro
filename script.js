bbd8934b02ae92ce7adeb500eec8695614775ee7
Marcelo <marceloreis7777@gmail.com>
2020-08-05 22:40:38 -0300

    Second commit

--- script.js
+++ script.js
@@ -1,83 +1,143 @@
-//run
 function main(){
- populateList()
- //Funcoes
- function populateList(){
-     //atribuicao do construct array, que inicializar o array com 100 posicoes
-     //e usa uma funcao map, que roda para cada elemento
-     const data = Array.from({length: 100}).map((_,i) => `<div class="item"> Item ${(i + 1)}</div>`)
- 
-     const lista = document.querySelector('#paginate .list')
-     lista.innerHTML = data.join("")
- 
-     return data
- }
- 
- const data = populateList()
- let perPage = 3
- //Guarda o estado da pagina
- const state = {
-     page: 1,
-     perPage,
-     totalPage: Math.ceil(data.length / perPage)
- }
- 
- const html = {
-     get(element){
-         console.log('oi')
-         return document.querySelector(element)
-     }
- }
- 
- //objeto de funçoes
- const controls = {
-     next() {
-         state.page++
-         const lastPage = state.page > state.totalPage
-         if(lastPage){
-             state.page--
-         }
-     },
-     prev() {
-         state.page--
-         if(state.page < 1){
-             state.page++
-         }
-     },
-     goTo(page) {
-         if(page < 1){
-             page = 1
-         }
-         state.page = page
- 
-         if(page > state.totalPage){
-             state.page = state.totalPage
-         }
-     },
-     createListeners(){
-        html.get('.first').addEventListener('click',()=>{
-            controls.goTo(1)
-            update()
-        })
+    //Funcoes
+    const data = Array.from({length: 100}).map((_,i) => `Item ${(i + 1)}`)
+    let perPage = 5
+    
+    //Guarda o estado da pagina
+    const state = {
+        page: 1,
+        perPage,
+        totalPage: Math.ceil(data.length / perPage),
+        maxVisibleButtons: 5
+    }
+    
+    const html = {
+        get(element){
+            return document.querySelector(element)
+        }
+    }
 
-        html.get('.last').addEventListener('click',()=>{
-            controls.goTo(state.totalPage)
-            update()
-        })
+    //objeto de funçoes
+    const controls = {
+        next() {
+            state.page++
+            const lastPage = state.page > state.totalPage
+            if(lastPage){
+                state.page--
+            }
+        },
+        prev() {
+            state.page--
+            if(state.page < 1){
+                state.page++
+            }
+        },
+        goTo(page) {
+            if(page < 1){
+                page = 1
+            }
+            state.page = +page
+    
+            if(page > state.totalPage){
+                state.page = state.totalPage
+            }
+        },
+        createListeners(){
+            html.get('.first').addEventListener('click',()=>{
+                controls.goTo(1)
+                update()
+            })
 
-        html.get('.next').addEventListener('click',()=>{
-            controls.next()
-            update()
-        })
+            html.get('.last').addEventListener('click',()=>{
+                controls.goTo(state.totalPage)
+                update()
+            })
 
-        html.get('.prev').addEventListener('click',()=>{
-            controls.prev()
-            update()
-        })
-     }
- }
+            html.get('.next').addEventListener('click',()=>{
+                controls.next()
+                update()
+            })
 
- function update(){
-     console.log(state.page)
- }
+            html.get('.prev').addEventListener('click',()=>{
+                controls.prev()
+                update()
+            })
+        }
+    }
+    const list = {
+        create(item) {
+            const div = document.createElement('div')
+            div.classList.add('item')
+            div.innerHTML = item
+            html.get('.list').appendChild(div)
+        },
+        update() {   
+            html.get('.list').innerHTML = ""
+
+            let page = state.page - 1
+            let start = page * state.perPage
+            let end = start + state.perPage
+            const paginatedItems = data.slice(start, end)
+            paginatedItems.forEach(list.create)
+        }
+    }
+    
+    const buttons = {
+        element: html.get('.numbers'),
+        create(number){
+            const button = document.createElement('div')
+            button.innerHTML = number
+            button.addEventListener('click', (event) =>{
+                const page = event.target.innerText
+
+                controls.goTo(page)
+                update()
+            })
+
+            if(state.page == number){
+                button.classList.add('active')
+            }
+            buttons.element.appendChild(button)
+        },
+        update(){
+            buttons.element.innerHTML = ""
+            const {maxLeft, maxRight} = buttons.calculateMaxVisible()
+
+            for(let page = maxLeft; page <= maxRight; page++){
+                buttons.create(page)
+            }
+        },
+        calculateMaxVisible(){
+            const { maxVisibleButtons} = state
+            let maxLeft = (state.page - Math.floor( maxVisibleButtons/2))
+            let maxRight = (state.page + Math.floor( maxVisibleButtons/2))
+            if(maxLeft < 1){
+                maxLeft = 1
+                maxRight = maxVisibleButtons
+            }
+
+            if(maxRight > state.totalPage){
+                maxLeft = state.totalPage - (maxVisibleButtons - 1)
+                maxRight = state.totalPage
+
+                if(maxLeft < 1){
+                    maxLeft = 1
+                }
+            }
+
+            return {maxLeft,maxRight}
+        }
+    }
+
+    function update(){
+        list.update(),
+        buttons.update()
+    }
+        
+    function init(){
+        list.update()
+        controls.createListeners()     
+    }
+
+    init()
 }
